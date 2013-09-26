38d1daa9460256f9c0bce32381a6b891d0a23402
James Torio <jamestorio@yahoo.com>
2013-09-26 18:25:27 -0400

    added login and log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_filter :configure_permited_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  protected
+  def configure_permited_parameters
+  devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:profile_name) }
+  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:last_name,
+  :profile_name, :first_name, :email, :password, :password_confirmation)}
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,13 @@
 ActiveRecord::Schema.define(version: 20130924142210) do
 
   create_table "statuses", force: true do |t|
-    t.string   "name"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "statuses", ["user_id"], name: "index_statuses_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "first_name"
