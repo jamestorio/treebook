77e162acd1bc96850533b2f9aa4977e1b629b31d
James Torio <jamestorio@yahoo.com>
2013-09-30 16:57:58 -0400

    styled the flash messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   
+  validates :first_name, presence: true
   has_many :statuses
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+      
          
   def full_name
     first_name + " " + last_name
