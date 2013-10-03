ad808ad74390e20c668ca38e0733cd53f7237757
James Torio <jamestorio@yahoo.com>
2013-10-02 21:38:29 -0400

    added fixtures, cant get a test to pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,15 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   
   
-  
-  
   validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true,
+                           uniqueness: true,
+                          format: {
+                            with: /\A[a-zA-Z0-9_-]+\Z/,
+                            message: "Must be formatted correctly"
+                           }
+  
   has_many :statuses
   
   devise :database_authenticatable, :registerable,
